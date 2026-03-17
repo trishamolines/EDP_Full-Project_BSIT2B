bf71f3e082abc0414f7e2b8f1a879db1b250d69a
trishamolines <trishamolines37@gmail.com>
2026-03-17 12:10:20 +0800

    first commit

---
+++ MOLINES_EDP/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MOLINES_EDP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[,] userCredentials =
+        {
+            { "admin", "admin", "trisha molines" },
+            { "cashier", "password", "sheldon cooper" }
+        };
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(tbUsername.Text == "")
+            {
+                MessageBox.Show("Please enter username:", "Validation");
+                tbUsername.Focus();
+            }
+            else if(tbPassword.Text == "")
+            {
+                MessageBox.Show("Please enter username:", "Validation");
+                tbUsername.Focus();
+            }
+            else
+            {
+                for (int x = 0; x < userCredentials.GetLength(0); x++)
+                {
+                    if (tbUsername.Text == userCredentials[x, 0])
+                    {
+                        if (tbPassword.Text == userCredentials[x, 1])
+                        {
+
+                            frmHome frm = new frmHome();
+                            MessageBox.Show("Welcome " + userCredentials[x, 2]);
+                            this.Hide();
+                            frm.Show();
+                            break;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid Username/Password");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MOLINES_EDP/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MOLINES_EDP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MOLINES_EDP")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7d4c2bcb-b0e0-43f8-9a27-e20e756bacec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
